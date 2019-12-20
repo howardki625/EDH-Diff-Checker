2ac4a724360c60b7f0e353c5d8d635b0546e7576
Howard <howardki625@gmail.com>
2019-12-20 14:34:56 -0500

    some comments

--- edhDiff.go
+++ edhDiff.go
@@ -1,3 +1,9 @@
 package main
 
-func main() {}
+func main() {
+	// takes 2 links as arguments
+	// goes to find the raw text of the decklists
+	// parse them into 2 arrays
+	// compare the 2 arrays
+	// lists the items that don't exist in the other
+}
